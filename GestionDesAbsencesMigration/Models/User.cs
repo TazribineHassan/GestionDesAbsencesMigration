103b9aaa38bb754bf3a0a14e0bc6b5081415e49c
Tazribine Hassan <tazribinehassan1998@gmail.com>
2021-06-17 01:55:13 +0100

    first commit

---
+++ GestionDesAbsencesMigration/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionDesAbsencesMigration.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [StringLength(50)]
+        public string Nom { get; set; }
+        
+        [Required]
+        public string Prenom { get; set; }        
+        
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        
+    }
+}
